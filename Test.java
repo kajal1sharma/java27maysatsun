83a9a6786efb59ddd04b67528d36c574003cd7b0
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-27 14:43:51 +0530

    ..

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test {
+
+    public static void main(String[] args){
+        //keywords => certain words that make the basic java constructs
+        //identifiers => are names of class ,variables, enums, inteface, fucntions..
+        //Test, main , String, args 
+
+        System.out.println("hellow this is our first class");
+    }
+}
