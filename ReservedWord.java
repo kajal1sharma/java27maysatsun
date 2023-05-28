2c082955d1d09f480b215ef3763477d292d31ced
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-28 14:30:53 +0530

    ..

---
+++ ReservedWord.java
@@ -0,0 +1,24 @@
+public class ReservedWord {
+    public static void main(String[] args) {
+        
+
+
+
+    }
+}
+
+
+
+//reserved words used as literals => true false null
+//reserved words => const ,goto (cannot use)
+//reserved words
+
+// =>control statements => 
+// if , else , continue,switch,case, return , break for , while ,do.....
+// =>data types
+// int byte short long float long char boolean
+// =>modifiers
+// public protected private ,static , final , abstract , synchronized...
+// exception =>try catch finally,throws, throw ,assert .......
+
+
--- Test.java
+++ Test.java
@@ -2,10 +2,36 @@ class Test {
 
     public static void main(String[] args) {
         
-        System.out.println("hello world");
-        System.out.println("hellow world 2");
-        System.out.println("hello world");
-        System.out.println("hellow world 2");
+
+        // int String =80;
+        // int main=79;
+       // System.out.println(main);
+    //    int public =90;
+       //identifiers => name of class , method, varibles, interface
+
+       int a =90;
+       //a => name of the variable
+
+       //a-z, A-Z,  0-9, $,_
+       int number;
+       int $number;
+       int Number;
+       int Number1;
+       //int 1number; => you cannot use digit as a first character while naming a identifier
+       int xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooodfsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas=90;
+       //camelCase => 
+       int firstNumber;
+       int first_number;
+       //identifiesr/Java are case sensitive
+
+
+       int secondNumber=90;
+       int SecondNumber=80;
+       //reserved words cannot be used as a identifiesr
+
+       //int if=90;
+
+       
 
 
     }
