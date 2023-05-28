69a7a5f70d9555cdabfde9bca882a803e5b0045a
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-28 15:31:54 +0530

    ..

--- ReservedWord.java
+++ ReservedWord.java
@@ -3,6 +3,52 @@ public class ReservedWord {
         
 
 
+        //floating data types
+
+
+        double d=12.34;
+        double maxDouble =Double.MAX_VALUE;
+        double minDouble =Double.MIN_VALUE;
+        System.out.println(maxDouble);
+        System.out.println(minDouble);
+
+        // float f=12.4f;//(4bytes)
+        // System.out.println(f);
+        // float floatMax =Float.MAX_VALUE;
+        // float floatMin =Float.MIN_VALUE;
+        // System.out.println(floatMax);
+        // System.out.println(floatMin);
+
+
+        //integral data types
+
+        // long maxLong = Long.MAX_VALUE;
+        // long minLong = Long.MIN_VALUE;
+        // System.out.println(maxLong);
+        // System.out.println(minLong);
+
+        /*int int_max = 2147483647;
+        int int_min = -2147483648;
+
+        System.out.println(int_max);
+        System.out.println(int_min);
+        */
+
+    //    short shortMax = Short.MAX_VALUE;
+    //    short shortMin = Short.MIN_VALUE;
+
+    //    System.out.println(shortMax);
+    //    System.out.println(shortMin);
+        
+
+
+        // byte byteMax= Byte.MAX_VALUE;
+        // byte byteMin =Byte.MIN_VALUE;
+
+        // System.out.println(byteMax);
+        // System.out.println(byteMin);
+
+        
 
     }
 }
