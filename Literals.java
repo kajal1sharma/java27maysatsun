243b0da19ca50eadd62dd0fc7dcf9aae6ac1d770
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-04 15:25:10 +0530

    ..

---
+++ Literals.java
@@ -0,0 +1,31 @@
+public class Literals {
+	public static void main(String []args) {
+//		char c='a';
+//		char c2=97;
+//		System.out.println(c2);
+//		
+//		
+//		//integral literals
+//		
+//		int a= 23;//decimal literal (0-9)
+//		int octalLiteral =027;// (0-7)
+//		int hexadecimalLit = 0X34abfe;//(0-15)(0-9, a, b , c, d, e,f)
+//		int binLiteral= 0b1010101; //(0-1)
+//		System.out.println(a);
+//		System.out.println(octalLiteral);
+//		System.out.println(hexadecimalLit);
+//		System.out.println(binLiteral);
+		
+		
+		boolean b= true;
+		boolean b2=false;
+		
+		
+		float f=34.34f;
+		float f2= 23e34f;
+		System.out.println(f);
+		System.out.println(f2);
+		
+	}
+}
+
