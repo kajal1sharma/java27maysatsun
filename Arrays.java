e2a4b0989581886539119eb0f2e1b613e0cd726c
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-04 15:27:52 +0530

    ..

---
+++ Arrays.java
@@ -0,0 +1,29 @@
+
+
+public class Arrays {
+	
+	public static void main(String []args) {
+		//arrays are group of similar type of data
+		
+		int student1 = 45;
+		int student2 =67;
+		int student3 =98;
+		int student4 =78;
+		int student5 =98;
+		//int sum = student1 + student2+student3.. . . . .student60;
+		
+		int []marks = {23,45,66,44,23,4};
+		
+		marks[0]=98;
+		marks[1]=97;
+		marks[2]=67;
+		marks[5]=70;
+		
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
+		System.out.println(marks[5]);
+		
+	}
+	
+}
