027b0d05a22c8b388f777f6238846140cf778a6b
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-24 18:59:44 +0530

    ..

---
+++ forLoop.java
@@ -0,0 +1,140 @@
+public class forLoop {
+
+    public static void print(int a){
+
+    }
+
+    public static void main(String[] args) {
+        //for loop
+        //hello world 5
+     
+
+        // System.out.println("hello world");
+        // System.out.println("hello world");
+        // System.out.println("hello world");
+        // System.out.println("hello world");
+        // System.out.println("hello world");
+
+        // for(int i=0; i<5; i=i+1){
+        //     System.out.println("hello world");
+        // }
+
+        //i=0
+        //i=1
+        //i=2
+        //i=3
+        //i=4
+        //i=5
+        
+        
+        // for( int i=0;i<5;i=i+5){
+        //     System.out.println("hello world");
+        // }
+        //table of 7
+
+        // for(int i=0;i<=70 ;i=i+7){
+        //     System.out.println(i);
+        // }
+        //7*1=7
+        //7*2=14
+
+        // String s= 13+1+"some"+"thing"+56+47;
+                
+        
+        // for(int i=1;i<=10;i++){
+        //     System.out.println("7 * "+i+" = "+i*7);
+        // }
+        
+
+        //1+2+...+100 =sum
+
+        // int sum =0;
+        // for(int i=1;i<=100;i++){
+        //     sum=sum+i;
+        // }
+        
+        
+        //sum=0+1
+        //sum=1+2;
+        //sum=3+3
+        //sum=6+4
+        //sum=10+5;
+        //sum=15+6
+        //sum=21+7....
+        //sum=x+100
+
+
+        //2  divisible sum
+        // for(int i=0;i<=100;i=i+2){
+        //     sum=sum+i;
+        // }
+
+        
+        // int sum2=0;
+        // for(int i=1;i<=100;i=i+1){
+        //     if(i%2==0 || i%3==0 || i%5==0){
+        //         sum2=sum2+i;
+        //     }
+        // }
+        // //sum2=2+3+4+5+6+8+9+10+12+14+15+16+..........
+        // System.out.println(sum2);
+
+        /*
+       |*|
+       |*|*|
+       |*|*|*|
+       |*|*|*|*|
+       |*|*|*|*|*|
+       |*|*|*|*|*|*|
+         */
+
+
+         //it for rows
+         for(int i=0;i<6;i++){
+
+            //it is for star printing
+            for(int j=0 ;j<=i ;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+         }
+       
+
+
+         /*
+       
+         *****
+         ****
+         ***
+         **
+         *
+         
+
+            *
+           * *
+          * * *
+         * * * *
+        * * * * *
+         
+
+        A B C D E
+        A B C D
+        A B C
+        A B
+        A
+
+
+        A
+        BC
+        DEF
+        GHIJ
+        KLMNO
+        PQRSTU
+          */
+
+    }
+}
+
+
+
+
--- operators.java
+++ operators.java
@@ -2,15 +2,55 @@ import java.util.Scanner;
 public class operators {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        boolean rain= sc.nextBoolean();
-        boolean umbrella= sc.nextBoolean();
-        if(!rain || (rain==true && umbrella==true)){
-            System.out.println("can go out");
-        }
-        else{
-            System.out.println("cannot go out");
-        }
+        // Scanner sc = new Scanner(System.in);
+
+        // int numberOne = sc.nextInt();
+        // int numberTwo =sc.nextInt();
+        // int numberThree = sc.nextInt();
+
+        // if(numberOne>numberTwo && numberOne>numberThree){
+        //     System.out.println(numberOne);
+        // }
+        // else if(numberTwo> numberOne && numberTwo>numberThree){
+        //     System.out.println(numberTwo);
+        // }
+        // else if(numberThree>numberOne && numberThree>numberTwo){
+        //     System.out.println(numberThree);
+        // }
+        // else {
+        //     System.out.println("no single greatest number");
+        // }
+
+
+
+
+
+        // boolean rain= sc.nextBoolean();
+        // boolean umbrella= sc.nextBoolean();
+        // boolean raincoat= sc.nextBoolean();
+
+        // if(rain==false){//!rain
+        //     System.out.println("you can go out as it is not raining");
+        // }
+        // else if(rain && umbrella){
+        //     System.out.println("you can go out with an umbrella");
+        // }
+        // else if(rain ==true && raincoat == true){
+        //     System.out.println("you can go out with raincoat");
+        // }
+        // else{
+        //     System.out.println("you can not go out");
+        // }
+
+
+
+
+        // if(!rain || (rain==true && (umbrella==true||raincoat==true))){
+        //     System.out.println("can go out");
+        // }
+        // else{
+        //     System.out.println("cannot go out");
+        // }
         //logical operators
         // operandLeft  && operandRight 
         // true(1)    &&(*)    true(1)  =true
