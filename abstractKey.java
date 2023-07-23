63d1ca7d97c4ef24d5ad0ac6b5694e38779a5642
brainmentorspvtltd <kajal1231995@gmail.com>
2023-07-23 14:31:04 +0530

    ..

---
+++ abstractKey.java
@@ -0,0 +1,33 @@
+
+abstract class Vehicle{
+    int seat;
+    int fuelCapacity;
+    int runningCapacity;
+    int wheels;
+
+    void distanceTravelled(){
+        System.out.println("n amount of distance has been travelled by the vehicle");
+    }
+
+   abstract void ModeOfTRansport();
+}
+
+abstract class TwoWheeler extends Vehicle{
+   abstract void ModeOfTRansport();
+}
+
+class MotorCycle extends TwoWheeler{
+    void ModeOfTRansport(){
+        System.out.println("land");
+     }
+}
+
+
+public class abstractKey {
+public static void main(String[] args) {
+  //  Vehicle v= new Vehicle();
+   // v.ModeOfTRansport();
+    MotorCycle m = new MotorCycle();
+    m.ModeOfTRansport();
+}    
+}
