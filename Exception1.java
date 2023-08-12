cd7da524ff82cb3a307bdfa908b5b034767fc446
kajal <kajal@gmail.com>
2023-08-12 16:18:53 +0530

    ..

--- Exception1.java
+++ Exception1.java
@@ -1,20 +1,60 @@
 public class Exception1 {
+    public static void doSomethingEvenMore(){
+        // try{
+            int a=90/0;
+            System.out.println("line 2 of do something even more");
+        // }
+        // catch(ArithmeticException e){
+        //     System.out.println("here the exception arrived");
+        // }
+    }
+    public static void doSomethingMore(){
+        System.out.println("line 1 in do something more ");
+        // try{
+        doSomethingEvenMore();
+        // }
+        // catch(Exception e){
+        //     System.out.println("Exception has been handled");
+        // // }
+        System.out.println("line 2 in dosomething more");
+    }
+    public static void doSomething(){
+        System.out.println("line 1 in doSomething");
+       // try{
+        doSomethingMore();
+        // }
+        // catch(Exception e){
+        //     System.out.println("Exception has been handled");
+        // }
+        System.out.println("line 2 in doSomethign");
+    }
     public static void main(String[] args) {
+
+        System.out.println("line 1 in main");
+       try{
+        doSomething();
+       }
+        catch(Exception e){
+            System.out.println("Exception has been handled");
+        }
+        System.out.println("line 2 in main");
+
+
 
         // Exception e= new ArithmeticException();
         // Object obj =new Exception();
         // Object obj2= new Exception1();
-        try{
-           int a=90;
-           int b=0;
-           int c=a/b; 
-           System.out.println("hello world 1");
-           System.out.println("Hello worldm 2");
-        }
-        catch(Exception e){
-            System.out.println(e);
-            System.out.println("hello world");
-        }
-        System.out.println("iam outside catch block");
+        // try{
+        //    int a=90;
+        //    int b=0;
+        //    int c=a/b; 
+        //    System.out.println("hello world 1");
+        //    System.out.println("Hello worldm 2");
+        // }
+        // catch(Exception e){
+        //     System.out.println(e);
+        //     System.out.println("hello world");
+        // }
+        // System.out.println("iam outside catch block");
     }
 }
