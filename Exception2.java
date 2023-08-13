bf41eac6ef47fe1b73cef4033a12522bc7a53e51
kajal <kajal@gmail.com>
2023-08-13 15:20:48 +0530

    ..

--- Exception2.java
+++ Exception2.java
@@ -3,25 +3,55 @@
 public class Exception2 {
     public static void main(String[] args) {
        
-        try{
-            // int a=90/0;
-            // String s="";
-            // s=null;
-            // s.equals("kdfsknf");
-            int []arr =new int[6];
-            arr[9]=90;
 
+        try{
+            int i=90;
         }
+        catch(Exception e){
+            System.out.println("i have handled it");
+        }
+        finally{
+            //clean up codes
+            System.out.println("this block will always run whethher the exception comes or not");
+        }
+
+
+
+
+        // try{
+        //     int a=0;
+            
+        // }
+        // catch(Exception e){
+        //     try{
+
+        //     }
+        //     catch(Exception e1){
+
+        //     }
+        // }
+
+
+
+        // try{
+        //     // int a=90/0;
+        //     // String s="";
+        //     // s=null;
+        //     // s.equals("kdfsknf");
+        //     int []arr =new int[6];
+        //     arr[9]=90;
+
+        // }
         
-        catch(ArithmeticException e){
-            System.out.println("this is arithmetic block");
-        }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("this is array block");
-        }
-         catch(Exception e){
-            System.out.println(e);
-        }
+        // catch(ArithmeticException e){
+        //     System.out.println("this is arithmetic block");
+        // }
+        // catch(ArrayIndexOutOfBoundsException e){
+        //     System.out.println("this is array block");
+        // }
+        //  catch(Exception e){
+        //     System.out.println(e);
+        // }
        
     }
 }
