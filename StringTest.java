40269245a29f05e94020c9ea758295f83a35b608
kajal <kajal@gmail.com>
2023-08-13 14:40:34 +0530

    ..

--- StringTest.java
+++ StringTest.java
@@ -1,25 +1,80 @@
 //java.lang package is by default available in all java 
+
+import java.util.Scanner;
+
 public class StringTest {
     public static void main(String[] args) {
+//reverse => this is a string ans=> gnirt a si siht
+
+        // Scanner sc =new Scanner(System.in);
+
+        // String str=sc.nextLine();
 
 
+        // String rev="";
+        // for(int i=0;i<str.length();i++){
+        //     rev=str.charAt(i)+rev;
+        // }
+        // System.out.println(rev);
 
-        String arr [] ={"abc.txt","script.js","Test.java","abc.py","Operators.java"};
-        int len = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i].contains(".java")){
-                len++;
-            }
-        }
-        String ans[] = new String[len];
-        int j=0;
-        for(int i=0;i<arr.length;i++){
-             if(arr[i].contains(".java")){
-                ans[j]=arr[i];
-                System.out.println(ans[j]);
-                j++;
-            }
-        }
+
+//reverse words => this is a string  ans=> string a is this
+            // String str ="this is a string";
+            // String ans[]=str.split(" ");
+            // String res="";
+
+            // for(int i=0;i<ans.length;i++){
+            //     res=ans[i]+" "+res;
+
+            // }
+            // System.out.println(res);
+            
+
+//palindrome abccba =>true , abcdca=>false
+                String str ="abctba";
+                
+                int j=str.length()-1;
+                boolean flag=true;
+                int i=0;
+                while(i<j){
+                    if(str.charAt(i)==str.charAt(j)){
+                        i++;
+                        j--;
+                    }
+                    else{
+                        flag=false;
+                        break;
+                    }
+                }
+
+                System.out.println(flag);
+                //String rev="";
+                // for(int i=0;i<str.length();i++){
+                //     rev=str.charAt(i)+rev;
+                // }
+                // if(rev.equals(str)){
+                //     System.out.println(true);
+                // }
+                // else{
+                //     System.out.println(false);
+                // }
+
+        // String arr [] ={"abc.txt","script.js","Test.java","abc.py","Operators.java"};
+        // int len = 0;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i].contains(".java")){
+        //         len++;
+        //     }
+        // }
+        // String ans[] = new String[len];
+        // int j=0;
+        // for(int i=0;i<arr.length;i++){
+        //      if(arr[i].contains(".java")){
+        //         ans[j]=arr[i];
+        //         System.out.println(ans[j]);
+        //         j++;
+        //     }
+        // }
 
 
 
@@ -51,7 +106,7 @@ public class StringTest {
     
 
 
-        int a=90;//literals are the constant values
+       // int a=90;//literals are the constant values
         // String str="test string";//literal way of defining string obj
         // String str1 = new String("test string ");
 
