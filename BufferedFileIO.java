2e0995f52424b746750b49e4d8349ae105df4e86
kajal <kajal@gmail.com>
2023-09-03 15:13:31 +0530

    ..

--- BufferedFileIO.java
+++ BufferedFileIO.java
@@ -1,22 +1,30 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class BufferedFileIO {
     public static void main(String[] args) throws FileNotFoundException, IOException{
-        FileReader f = new FileReader("abc.txt");
-        BufferedReader br =new BufferedReader(f);
+        FileWriter fw = new FileWriter("abc.txt", true);
+        fw.write("\nthis is line 14");
+        fw.close();
+        
 
-        // String s = br.readLine();
-        // while(s!=null){
-        //     System.out.println(s);
-        //     s= br.readLine();
-        // }
+        
+        //FileReader f = new FileReader("abc.txt");
+        //  BufferedReader br =new BufferedReader(f);
+
+        //  String s = br.readLine();
+        // // while(s!=null){
+        // //     System.out.println(s);
+        // //     s= br.readLine();
+        // // }
+
 
         
 
-        br.close();
-        f.close();
+        // br.close();
+        // f.close();
     }
 }
